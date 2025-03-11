cc2f55fd454b8cb4adb97937379b53e07e49d236
devzoofi <devzoofi@users.noreply.github.com>
2025-03-11 05:57:28 +0000

    deploy: af736d85c6bf97888330e9b75d7a26dd998b0c54

---
+++ _next/static/zUYKsLKNMxgXh6unU6J5W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-5498d079ec98c70a.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
